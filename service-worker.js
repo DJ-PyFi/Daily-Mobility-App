c490227c536f26c197c9da229efa4c8838bcb250
DJ <johanssondavidj@gmail.com>
2026-08-08 17:16:42 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this version any time app files change, to invalidate old caches.
-const CACHE_VERSION = 'ten-min-mobility-v1';
+const CACHE_VERSION = 'ten-min-mobility-v3';
 const CORE_ASSETS = [
   './',
   './index.html',
